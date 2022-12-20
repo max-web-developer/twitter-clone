de598a2533415836e10d56a2ba17ad1a647b3f5c
maksad <maxkavlanov110@gmail.com>
2022-12-20 19:16:45 +0500

    adding new

--- src/components/Main/index.tsx
+++ src/components/Main/index.tsx
@@ -23,7 +23,7 @@ const Main: React.FC = () => {
         </button>
 
         <ProfileInfo>
-          <strong>Elton Lazzarin</strong>
+          <strong>Kavlanov Max</strong>
           <span>432 Tweets</span>
         </ProfileInfo>
       </Header>
